8f4760dd2b69607db08a42c381049c557a1f3430
jojaehyeok99 <choco5366@naver.com>
2024-04-17 12:46:33 +0900

    2024 - 04 - 17 react : library - react-router  패키지(1)

--- libraries/src/App.tsx
+++ libraries/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import ZustandLibrary from 'src/zustand/ZustandLibrary';
+import { Route, Routes } from 'react-router';
+import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 
 function App() {
   return (
-  <>
-    <ZustandLibrary />
-  </>
+  <Routes>
+    {/* <Route index element={<ReactRouterLibrary />} /> 
+    <Route path='/page1' element={<ReactRouterPage1 />} />
+    <Route path='/page2' element={<ReactRouterPage2 />} />
+    <Route path='/zustand' element={<ZustandLibrary />} /> */}
+    <Route path='/router' >
+      <Route index element={<ReactRouterLibrary />} />
+      {/* 현재 path의 URL을 path='/router'로 설정했을때 path='/page1' '/' 빼기 */}
+      <Route path='page1' element={<ReactRouterPage1 />} />
+      <Route path='page2' element={<ReactRouterPage2 />} />
+    </Route>
+    <Route path='/zustand' element={<ZustandLibrary />} />
+  </Routes>
 );
 }
 
